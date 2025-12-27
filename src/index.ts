4467f78aa4b2539db88efec623196412a0ff5000
Mostafa Sarhan <mostafasarhan@Mostafas-MacBook-Pro.local>
2025-12-27 12:01:32 +0200

    Udate corn

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { orderModel } from "./models/order";
 import { deliveryModel } from "./delivery";
-import "./archiveOrder.ts";
+import "./archiveOrder";
 import { merchantModel } from "./merchantModel";
 import { Request, Response, NextFunction } from "express";
 import crypto from "crypto";
